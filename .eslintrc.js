5f09b9d09d2b5ab2ebf6b86d8d8edb687dcbf8d9
zhiyuzy.chen <zhiyuzy.chen@qunar.com>
2023-10-29 15:11:49 +0800

    test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,5 @@ module.exports = {
     parserOptions: {
         ecmaVersion: 2021, // 根据你的项目需要选择适当的版本
     },
-    extends: [
-        // "@qnpm/eslint-config-qunar-base",
-        'plugin:diff/diff',
-    ],
+    extends: ['@qnpm/eslint-config-qunar-base', 'plugin:diff/diff']
 };
