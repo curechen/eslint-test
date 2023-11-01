a5208e54f3353927ac5a5460f4dca91b026c5781
zhiyuzy.chen <zhiyuzy.chen@qunar.com>
2023-11-01 10:55:25 +0800

    test

--- add.js
+++ add.js
@@ -10,8 +10,6 @@ const packageJson = require(packageJsonPath);
 
 const isNanachi = packageJson && packageJson.name && packageJson.name.indexOf('nnc') !== -1;
 
-let needCreateHuskyConfig = false;
-
 const defaultEslintIgnoreConfig = `
 node_modules
 coverage
@@ -30,8 +28,8 @@ packageJson.dependencies = packageJson.dependencies || {};
 packageJson.devDependencies = packageJson.devDependencies || {};
 
 handlePreCommitDependency();
-handleEslintConfFile();
-handleEslintIgnoreFile();
+// handleEslintConfFile();
+// handleEslintIgnoreFile();
 
 packageJsonChanged && fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
 
@@ -43,8 +41,10 @@ exec('npm install', (error) => {
         console.error(`\x1b[31mError executing npm install: ${error}\x1b[0m`);
     } else {
         console.log('执行 npm install 下载依赖完成');
-        packageJsonChanged ? console.log('成功添加 eslint，husky，lint-staged 相关依赖！') : console.log('package.json 中已存在 eslint，husky，lint-staged 相关依赖！');
-        needCreateHuskyConfig && createHuskyConfig();
+        packageJsonChanged
+            ? console.log('成功添加 eslint，husky，lint-staged 相关依赖！')
+            : console.log('package.json 中已存在 eslint，husky，lint-staged 相关依赖！');
+        createHuskyConfig();
     }
 });
 
@@ -52,56 +52,25 @@ exec('npm install', (error) => {
  * 提交前检测需要依赖 husky/pre-commit + lint-staged，该函数目的就是为了判断当前项目中是否存在这些依赖，不存在则添加
  */
 function handlePreCommitDependency() {
-    // 检查是否已经存在 eslint、husky 和 lint-staged 依赖
+    handleEslintDependency();
+    handleHuskyDependency();
+    handleLintStagedDependency();
+    handleEslintPluginDiffDependency();
+    handleNanachiDependency();
+}
+
+/**
+ * 处理 eslint 相关依赖及配置
+ */
+function handleEslintDependency() {
     const hasEslint = packageJson.devDependencies.eslint;
-    const hasHusky = packageJson.devDependencies.husky;
-    const hasPreCommit = packageJson.devDependencies['pre-commit'];
-    const hasLintStaged = packageJson.devDependencies['lint-staged'];
-
-    // 项目中只要缺少其中一种依赖则在 package.json 中添加配置并安装，husky 和 pre-commit 作用一致，不同项目使用的不同所以在这里是二选一
-    if (!hasEslint || (!hasHusky && !hasPreCommit) || !hasLintStaged) {
-        // 添加 eslint、husky 和 lint-staged 依赖
-
-        // 只有当两者都没有的时候再去安装 husky，否则就默认是使用项目中原先的依赖，对于 nanachi 来说，执行 npm run wx 会安装 pre-commit，所以在这里也不做安装
-        if (!hasHusky && !hasPreCommit && !isNanachi) {
-            packageJson.devDependencies.husky = '^3.0.5';
-            // 有则替换 prepare 命令，无则添加
-            // prepare 钩子会在 npm install 前执行
-            packageJson.scripts = packageJson.scripts || {};
-            // packageJson.scripts.prepare = "npx husky install && npx husky add .husky/pre-commit 'npx lint-staged'";
-            needCreateHuskyConfig = true;
-        }
-        if (!hasLintStaged) packageJson.devDependencies['lint-staged'] = '^9.2.5';
-
-        // 添加 husky 和 lint-staged 配置
-        // packageJson.husky = {
-        //     hooks: {
-        //         'pre-commit': 'lint-staged',
-        //     },
-        // };
-
-        packageJson['lint-staged'] = {
-            // 'src/**/*.{js,jsx}': ['eslint'],
-            // 'source/**/*.{js,jsx}': ['eslint'],
-            '*.{js,jsx}': ['eslint'],
-        };
-
-        // 对 nanachi 项目单独进行配置
-        if (isNanachi) {
-            packageJson['pre-commit'] = ['lint-staged'];
-            packageJson.scripts['lint-staged'] = 'lint-staged';
-            if (!hasEslint) packageJson.devDependencies.eslint = '^5.6.1';
-            packageJson.devDependencies['eslint-config-airbnb-base'] = '^15.0.0';
-            packageJson.devDependencies['eslint-plugin-import'] = '^2.29.0';
-            packageJson.devDependencies['babel-eslint'] = '^10.0.1';
-        } else {
-            packageJson.devDependencies['eslint-plugin-diff'] = '1.0.15'; // 适配低版本 node
-        }
-
+    // 只有 nanachi 需要指定 eslint 版本，其他项目可以通过引入 base 文件来引入 eslint，不需要额外添加
+    if (isNanachi && !hasEslint) {
+        packageJson.devDependencies.eslint = '^5.6.1';
         packageJsonChanged = true;
-    } else {
-        console.log('package.json 中已存在 eslint，husky，lint-staged 相关依赖！');
     }
+    handleEslintConfFile();
+    handleEslintIgnoreFile();
 }
 
 /**
@@ -123,24 +92,38 @@ function handleEslintConfFile() {
         // let type = args && args[0] && args[0].slice(1);
         // const depend = (type && extendList[type]) || extendList.base;
         let config = {};
-        if (isNanachi) {
+
+        const type = args && args[0] && args[0].slice(1);
+
+        if (isNanachi || type === 'normal') {
+            // nanachi 单独写配置
             config = { extends: ['eslint-config-airbnb-base'], parser: 'babel-eslint' };
+        } else if (type) {
+            const depend = type && extendList[type];
+            config = { extends: [depend, 'plugin:diff/diff'] };
+            packageJson.devDependencies[depend] = 'latest';
         } else {
             const extendRules = [];
             const isTypescript = 'typescript' in packageJson.dependencies;
             if ('react' in packageJson.dependencies) {
-                isTypescript ? extendRules.push(extendList.ts_react) : extendRules.push(extendList.react);
+                isTypescript
+                    ? extendRules.push(extendList.ts_react)
+                    : extendRules.push(extendList.react);
             }
             if ('qunar-react-native' in packageJson.dependencies) {
                 isTypescript ? extendRules.push(extendList.ts_rn) : extendRules.push(extendList.rn);
             }
 
             if (packageJson && packageJson.name && packageJson.name.indexOf('node') !== -1) {
-                isTypescript ? extendRules.push(extendList.ts_node) : extendRules.push(extendList.node);
+                isTypescript
+                    ? extendRules.push(extendList.ts_node)
+                    : extendRules.push(extendList.node);
             }
 
-            if (extendRules.length === 0 && !isNanachi) {
-                isTypescript ? extendRules.push(extendList.ts_base) : extendRules.push(extendList.base);
+            if (extendRules.length === 0) {
+                isTypescript
+                    ? extendRules.push(extendList.ts_base)
+                    : extendRules.push(extendList.base);
             }
 
             // const config = { extends: [depend, 'plugin:diff/diff'] };
@@ -157,6 +140,8 @@ function handleEslintConfFile() {
         } catch (err) {
             console.error(`\x1b[31m写入配置文件时发生错误: ${err}\x1b[0m`);
         }
+    } else {
+        console.log('eslint 配置文件已存在');
     }
 }
 
@@ -194,13 +179,87 @@ function handleEslintIgnoreFile() {
     }
 }
 
+/**
+ * 处理 husky 相关依赖及配置
+ */
+function handleHuskyDependency() {
+    const hasHusky = packageJson.devDependencies.husky;
+    if (!hasHusky) {
+        packageJson.devDependencies.husky = '^3.0.5';
+        // 有则替换 prepare 命令，无则添加
+        // prepare 钩子会在 npm install 前执行
+        // packageJson.scripts = packageJson.scripts || {};
+        // packageJson.scripts.prepare = "npx husky install && npx husky add .husky/pre-commit 'npx lint-staged'";
+
+        // packageJson.husky = {
+        //     hooks: {
+        //         'pre-commit': 'lint-staged',
+        //     },
+        // };
+        packageJsonChanged = true;
+    }
+    // 执行完 npm install 后再执行创建配置
+    // createHuskyConfig();
+}
+
 function createHuskyConfig() {
-    // 执行 npm install 来安装新的依赖
-    exec("npx husky install && npx husky add .husky/pre-commit 'npx lint-staged'", (error) => {
-        if (error) {
-            console.error(`\x1b[31mError executing npm install: ${error}\x1b[0m`);
-        } else {
-            console.log('husky 配置文件创建成功');
+    const huskyFolder = './.husky';
+    // 检查是否存在.husky文件夹
+    if (!fs.existsSync(huskyFolder)) {
+        // 如果不存在，执行创建命令
+        console.log('正在创建 husky 配置文件，请等待');
+        // 执行 npm install 来安装新的依赖
+        exec("npx husky install && npx husky add .husky/pre-commit 'npx lint-staged'", (error) => {
+            if (error) {
+                console.error(`\x1b[31mError executing npm install: ${error}\x1b[0m`);
+            } else {
+                console.log('husky 配置文件创建成功');
+            }
+        });
+    } else {
+        console.log('.husky文件夹已存在');
+    }
+}
+
+/**
+ * 处理 lint-staged 相关依赖及配置
+ */
+function handleLintStagedDependency() {
+    const hasLintStaged = packageJson.devDependencies['lint-staged'];
+    if (!hasLintStaged) {
+        packageJson.devDependencies['lint-staged'] = '^9.2.5';
+        packageJsonChanged = true;
+    }
+    packageJson['lint-staged'] = {
+        // 'src/**/*.{js,jsx}': ['eslint'],
+        // 'source/**/*.{js,jsx}': ['eslint'],
+        '*.{js,jsx}': ['eslint'],
+    };
+}
+
+/**
+ * 处理 eslint-plugin-diff 相关依赖及配置
+ */
+function handleEslintPluginDiffDependency() {
+    // nanachi eslint 版本不兼容，所以不使用该插件
+    if (!isNanachi) {
+        packageJson.devDependencies['eslint-plugin-diff'] = '1.0.15'; // 适配低版本 node
+        packageJsonChanged = true;
+    }
+}
+
+function handleNanachiDependency() {
+    const hasEslint = packageJson.devDependencies.eslint;
+    // 对 nanachi 项目单独进行配置
+    if (isNanachi) {
+        // packageJson['pre-commit'] = ['lint-staged'];
+        // packageJson.scripts['lint-staged'] = 'lint-staged';
+        if (!hasEslint) {
+            packageJson.devDependencies.eslint = '^5.6.1';
+            packageJsonChanged = true;
         }
-    });
+        packageJson.devDependencies['eslint-config-airbnb-base'] = '^15.0.0';
+        packageJson.devDependencies['eslint-plugin-import'] = '^2.29.0';
+        packageJson.devDependencies['babel-eslint'] = '^10.0.1';
+    }
 }
